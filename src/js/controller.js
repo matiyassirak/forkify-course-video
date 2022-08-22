6e9bcef2c702326e9013c129edaf61dbcaa89050
MatiyasPro <matiyasem23@gmail.com>
2022-08-22 07:25:00 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -119,6 +119,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -128,5 +132,6 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
   console.log('Welcome!');
+  newFeature();
 };
 init();
