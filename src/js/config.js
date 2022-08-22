1a12349dfb039c5fe10b8e2e7ff2bfc836ce010d
MatiyasPro <matiyasem23@gmail.com>
2022-08-22 07:16:26 +0300

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0175ef32-2bd3-4b53-85fe-e2b34cb83ef6';
+export const MODAL_CLOSE_SEC = 2.5;
